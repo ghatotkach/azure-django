78a954fdb268433068b13023a1f58459dac15fdd
ghatotkach <ghatotkach@users.noreply.github.com>
2022-04-30 19:05:24 +0300

    removed secret

--- DailyDairy/settings.py
+++ DailyDairy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bqeo#quf_g^0t$-&u&$uozui$o$7h3%d3wgv0k&91o!g&r@8w^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
